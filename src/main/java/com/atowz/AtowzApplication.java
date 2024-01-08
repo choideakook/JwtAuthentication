132b71b5cefe1d342ac495fd48967e8d0928cfef
sosow0212 <sosow0212@naver.com>
2024-01-08 12:27:57 +0900

    feat: 프로젝트 설정

---
+++ src/main/java/com/atowz/AtowzApplication.java
@@ -0,0 +1,13 @@
+package com.atowz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtowzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtowzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/atowz/AtowzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atowz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtowzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
