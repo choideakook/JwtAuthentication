72fa3f33a47b2a93e33c0044bd024b9ede6cdb39
choideakook <shdrnrhd113@naver.com>
2024-01-16 15:49:25 +0900

    test: jwt provider test 완료

---
+++ src/main/java/com/atowz/global/jwt/JwtProvider.java
@@ -0,0 +1,73 @@
+package com.atowz.global.jwt;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Base64;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Component
+public class JwtProvider {
+
+    private SecretKey cachedSecretKey;
+
+    @Value("${jwt.secret}")
+    private String secretKeyPlain;
+
+    private SecretKey _getSecretKey() {
+        String ketBase64Encoded = Base64.getEncoder().encodeToString(secretKeyPlain.getBytes());
+        return Keys.hmacShaKeyFor(ketBase64Encoded.getBytes());
+    }
+
+    public SecretKey getSecretKey() {
+        if (cachedSecretKey == null)
+            cachedSecretKey = _getSecretKey();
+        return cachedSecretKey;
+    }
+
+    public String getToken(Map<String, Object> claims, long seconds) {
+        long now = new Date().getTime();
+        Date tokenExpireIn = new Date(now + 1000L + seconds);
+
+        return Jwts.builder()
+                .claim("body", jsonToStr(claims))
+                .setExpiration(tokenExpireIn)
+                .signWith(getSecretKey(), SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    public Map<String, Object> getClaims(String token) {
+        String body = Jwts.parserBuilder()
+                .setSigningKey(getSecretKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .get("body", String.class);
+
+        return strToMap(body);
+    }
+
+    private Object jsonToStr(Map<String, Object> claims) {
+        try {
+            return new ObjectMapper().writeValueAsString(claims);
+        } catch (JsonProcessingException e) {
+            return null;
+        }
+    }
+
+    private Map<String, Object> strToMap(String jsonStr) {
+        try {
+            return new ObjectMapper().readValue(jsonStr, LinkedHashMap.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/test/java/com/atowz/global/jwt/JwtProviderTest.java
@@ -0,0 +1,50 @@
+package com.atowz.global.jwt;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("JWT 테스트")
+@SpringBootTest
+class JwtProviderTest {
+
+    @Autowired
+    JwtProvider jwtProvider;
+
+    @Test
+    @DisplayName("JWT 생성")
+    void no1() {
+        Map<String, Object> claims = createClaims();
+
+        String jwt = jwtProvider.getToken(claims, 60 * 10);
+
+        System.out.println("Token : " + jwt);
+    }
+
+    @Test
+    @DisplayName("JWT 복호화")
+    void no2() {
+        Map<String, Object> claims = createClaims();
+        String jwt = jwtProvider.getToken(claims, 60 * 10);
+
+        Map<String, Object> result = jwtProvider.getClaims(jwt);
+
+        assertThat(result.get("id")).isEqualTo(1);
+        assertThat(result.get("username")).isEqualTo("user");
+    }
+
+
+    private static Map<String, Object> createClaims() {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id", 1);
+        claims.put("username", "user");
+        return claims;
+    }
+}
