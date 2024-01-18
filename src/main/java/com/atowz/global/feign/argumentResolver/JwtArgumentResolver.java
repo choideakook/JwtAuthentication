6249eaa285f9c3a579797c81d00be941e1c885b7
choideakook <shdrnrhd113@naver.com>
2024-01-18 13:56:08 +0900

    feat: jwt 에서 member 를 조회하는 argument resolver 객체 생성

---
+++ src/main/java/com/atowz/global/feign/argumentResolver/JwtArgumentResolver.java
@@ -0,0 +1,32 @@
+package com.atowz.global.feign.argumentResolver;
+
+import com.atowz.auth.infrastructure.jwt.JwtService;
+import com.atowz.member.doamin.entity.Member;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+@RequiredArgsConstructor
+public class JwtArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final JwtService jwtService;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(JwtAuthorization.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
+
+        String accessToken = request.getHeader("Authorization");
+        return jwtService.getMember(accessToken);
+    }
+}
