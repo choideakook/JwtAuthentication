1cb2ec9932b759552398291608a79dad2bc2b73e
choideakook <shdrnrhd113@naver.com>
2024-01-24 11:44:52 +0900

    test: 기본테스트에 이름 추가

--- src/test/java/com/atowz/AtowzApplicationTests.java
+++ src/test/java/com/atowz/AtowzApplicationTests.java
@@ -1,8 +1,10 @@
 package com.atowz;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@DisplayName("통합 : 서버 실행")
 @SpringBootTest
 class AtowzApplicationTests {
 
